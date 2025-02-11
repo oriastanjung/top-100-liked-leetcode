3312933f340311082b17247d98bfd82bd0d0fc06
oriastanjung <oriastan999@gmail.com>
2025-02-11 10:50:14 +0700

    add two number solution

--- add_two_numbers.ts
+++ add_two_numbers.ts
@@ -31,59 +31,41 @@ function addTwoNumbers(
 ): ListNode | null {
   let firstDepth: Item[] = [];
   let secondDepth: Item[] = [];
-  
-  while (l1?.next) {
-    if (firstDepth.length === 0)
-      firstDepth.push({ val: l1?.val ? l1?.val : 0, depth: 1 });
-    else {
-      firstDepth.push({
-        val: l1?.val ? l1?.val : 0,
-        depth: firstDepth[firstDepth.length - 1].depth * 10,
-      });
-    }
 
-    l1 = l1?.next ? l1.next : null;
-    if (l1?.next === null) {
-      firstDepth.push({
-        val: l1?.val ? l1?.val : 0,
-        depth: firstDepth[firstDepth.length - 1].depth * 10,
-      });
-      break;
-    }
-  }
-  let firstTotal = 0;
-  for (let i = 0; i < firstDepth.length; i++) {
-    firstTotal += firstDepth[i].val * firstDepth[i].depth;
+  // Menangani node pertama sebelum loop
+  if (l1) firstDepth.push({ val: l1.val, depth: 1 });
+  if (l2) secondDepth.push({ val: l2.val, depth: 1 });
+
+  while (l1?.next) {
+    l1 = l1.next;
+    firstDepth.push({
+      val: l1.val,
+      depth: firstDepth[firstDepth.length - 1].depth * 10,
+    });
   }
 
   while (l2?.next) {
-    if (secondDepth.length === 0)
-      secondDepth.push({ val: l2?.val ? l2?.val : 0, depth: 1 });
-    else {
-      secondDepth.push({
-        val: l2?.val ? l2?.val : 0,
-        depth: secondDepth[secondDepth.length - 1].depth * 10,
-      });
-    }
+    l2 = l2.next;
+    secondDepth.push({
+      val: l2.val,
+      depth: secondDepth[secondDepth.length - 1].depth * 10,
+    });
+  }
 
-    l2 = l2?.next ? l2.next : null;
-    if (l2?.next === null) {
-      secondDepth.push({
-        val: l2?.val ? l2?.val : 0,
-        depth: secondDepth[secondDepth.length - 1].depth * 10,
-      });
-      break;
-    }
-  }
-  let secondTotal = 0;
-  for (let i = 0; i < secondDepth.length; i++) {
-    secondTotal += secondDepth[i].val * secondDepth[i].depth;
-  }
-  
+  let firstTotal = firstDepth.reduce(
+    (sum, item) => sum + item.val * item.depth,
+    0
+  );
+  let secondTotal = secondDepth.reduce(
+    (sum, item) => sum + item.val * item.depth,
+    0
+  );
+
   let total = firstTotal + secondTotal;
   let totalStr = total.toString();
   let totalArr = totalStr.split("").map((item) => Number(item));
-  let result
+
+  let result: ListNode | null = null;
   for (let i = 0; i < totalArr.length; i++) {
     result = new ListNode(totalArr[i], result);
   }
@@ -92,11 +74,11 @@ function addTwoNumbers(
 }
 
 // case 1
-const input = new ListNode(2, new ListNode(4, new ListNode(3)));
-const input2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+// const input = new ListNode(2, new ListNode(4, new ListNode(3)));
+// const input2 = new ListNode(5, new ListNode(6, new ListNode(4)));
 
 // case 2
-// const input = new ListNode(0);
-// const input2 = new ListNode(0);
+const input = new ListNode(0);
+const input2 = new ListNode(1);
 
-console.log(addTwoNumbers(input, input2))
+console.log(addTwoNumbers(input, input2));
